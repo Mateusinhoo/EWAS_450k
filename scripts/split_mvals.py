d262a5e3eefd15e138ac4e24cfb5d75dd5d8c6fe
Mateus Stocco <90330634+Mateusinhoo@users.noreply.github.com>
2025-07-09 19:24:55 -0600

    Update split_mvals.py

--- scripts/split_mvals.py
+++ scripts/split_mvals.py
@@ -1,20 +1,20 @@
 import pandas as pd
 
-# Load the full M-values matrix
-mvals = pd.read_csv("data/mvals.csv.gz", index_col=0).transpose()
+# Load M-values (samples are columns, CpGs are rows)
+mvals = pd.read_csv("data/mvals.csv.gz", index_col=0)
 
-# Load updated phenotype files
+# Load phenotype files
 pheno_blood = pd.read_csv("data/pheno_blood.csv")
 pheno_brain = pd.read_csv("data/pheno_brain.csv")
 
-# Extract sample IDs
+# Get sample IDs
 samples_blood = pheno_blood["sampleID"].tolist()
 samples_brain = pheno_brain["sampleID"].tolist()
 
-# Subset the mvals
-mvals_blood = mvals.loc[samples_blood].transpose()
-mvals_brain = mvals.loc[samples_brain].transpose()
+# Subset by columns (samples)
+mvals_blood = mvals[samples_blood]
+mvals_brain = mvals[samples_brain]
 
-# Save split files
+# Save to gzipped CSVs
 mvals_blood.to_csv("data/mvals_blood.csv.gz", compression="gzip")
 mvals_brain.to_csv("data/mvals_brain.csv.gz", compression="gzip")
